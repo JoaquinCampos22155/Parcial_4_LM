a8981d4f9ec522f5376fa10754a6db14ff81a973
Cesar Lopez <110710742+Czar272@users.noreply.github.com>
2024-11-10 19:16:58 -0600

    chore(prettier): clean code

--- MaquinaT.py
+++ MaquinaT.py
@@ -1,5 +1,15 @@
 class TuringMachine:
-    def __init__(self, states, alphabet, tape_alphabet, transitions, start_state, accept_state, reject_state, blank_symbol):
+    def __init__(
+        self,
+        states,
+        alphabet,
+        tape_alphabet,
+        transitions,
+        start_state,
+        accept_state,
+        reject_state,
+        blank_symbol,
+    ):
         self.states = states
         self.alphabet = alphabet
         self.tape_alphabet = tape_alphabet
@@ -20,42 +30,54 @@ class TuringMachine:
     def step(self):
         symbol = self.tape[self.head_position]
         if (self.current_state, symbol) in self.transitions:
-            new_state, new_symbol, direction = self.transitions[(self.current_state, symbol)]
+            new_state, new_symbol, direction = self.transitions[
+                (self.current_state, symbol)
+            ]
             self.tape[self.head_position] = new_symbol
             self.current_state = new_state
-            self.head_position += 1 if direction == 'R' else -1
+            self.head_position += 1 if direction == "R" else -1
             if self.head_position < 0:
                 self.head_position = 0
             elif self.head_position >= len(self.tape):
                 self.tape.append(self.blank_symbol)
         else:
-            return False  
+            return False
         return True
 
     def run(self):
         configurations = []
         visited_confs = set()
 
-        current_config = (self.current_state, ''.join(self.tape), self.head_position)
+        current_config = (self.current_state, "".join(self.tape), self.head_position)
         configurations.append(current_config)
         visited_confs.add(current_config)
 
-        while self.current_state != self.accept_state and self.current_state != self.reject_state:
+        while (
+            self.current_state != self.accept_state
+            and self.current_state != self.reject_state
+        ):
             if not self.step():
                 break
-            current_config = (self.current_state, ''.join(self.tape), self.head_position)
+            current_config = (
+                self.current_state,
+                "".join(self.tape),
+                self.head_position,
+            )
             configurations.append(current_config)
 
             if current_config in visited_confs:
                 break
 
             visited_confs.add(current_config)
-            
-        configurations.append((self.current_state, ''.join(self.tape), self.head_position))
+
+        configurations.append(
+            (self.current_state, "".join(self.tape), self.head_position)
+        )
         return configurations
 
     def save_configurations(self, filename, configurations):
-        with open(filename, 'w') as f:
+        with open(filename, "w") as f:
             for config in configurations:
-                f.write(f"State: {config[0]}, Tape: {config[1]}, Head Position: {config[2]}\n")
-
+                f.write(
+                    f"State: {config[0]}, Tape: {config[1]}, Head Position: {config[2]}\n"
+                )
